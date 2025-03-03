ff49ae5d11b3b6ac8f44f2627292aec89bb47cef
Raj Kiran Chaudhary <ebpearls@ebpadmins-Mac-mini-10.local>
2025-03-03 15:38:25 +0545

    authentication via email

--- src/pages/Email.jsx
+++ src/pages/Email.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { use, useState } from "react";
+import supabase from "../config/supabaseClient";
+import { Navigate } from "react-router-dom";
 
 const Email = () => {
+  const navigate = useNavigate();
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [inputError, setInputError] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -16,36 +22,69 @@ const Email = () => {
     }
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Name:", name);
-    console.log("Email:", email);
-    console.log("Password:", password);
+
+    if (!name || !email || !password) {
+      setInputError("Please fill in all fields");
+      return;
+    }
+
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        name: name,
+        email: email,
+        password: password,
+        options: {
+          emailRedirectTo: "http://localhost:5173/email",
+        },
+      });
+
+      if (error) {
+        console.log("Error during sign up: ", error);
+      }
+
+      if (data) {
+        console.log("Sign-up successful. Data:", data);
+        console.log("Confirmation email sent to:", email);
+        setInputError(null);
+      }
+    } catch (err) {
+      console.log("unexpected error: ", err);
+    }
   };
 
   return (
     <div className="page email">
       <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
         <input
           type="text"
-          placeholder="Name"
+          id="name"
           name="name"
+          value={name}
           onChange={handleChange}
         />
+        <label htmlFor="email">Email</label>
         <input
-          type="text"
-          placeholder="Email"
+          type="email"
+          id="email"
           name="email"
+          value={email}
           onChange={handleChange}
         />
+        <label htmlFor="password">Password</label>
         <input
           type="password"
-          placeholder="Password"
+          id="password"
           name="password"
+          value={password}
           onChange={handleChange}
         />
-        <button type="submit">Submit</button>
+        <button type="submit">Sign Up</button>
       </form>
+
+      {inputError && <p style={{ textAlign: "center" }}>{inputError}</p>}
     </div>
   );
 };
